5b20d62d668c87958c809c0d883afc069683c591
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-12 13:36:29 +0200

    Migrate to version 3.8

--- tests/test_product_images.py
+++ tests/test_product_images.py
@@ -3,28 +3,14 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     '''
     Test module.
     '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_images')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('product_images')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    module = 'product_images'
 
 
 def suite():
