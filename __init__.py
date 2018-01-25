c9b52acfd74ead985ac32e1a84120c1b84ec4cee
C?dric Krier <ced@b2ck.com>
2018-01-25 11:15:03 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 
 from trytond.pool import Pool
-from .product import *
+from . import product
 
 
 def register():
     Pool.register(
-        Attachment,
-        Template,
+        product.Attachment,
+        product.Template,
         module='product_images', type_='model')
--- product.py
+++ product.py
@@ -1,32 +1,25 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval
 
 __all__ = ['Attachment', 'Template']
-__metaclass__ = PoolMeta
 
 
 class Attachment:
+    __metaclass__ = PoolMeta
     __name__ = 'ir.attachment'
 
     product_image = fields.Boolean('Product Image')
 
 
 class Template:
+    __metaclass__ = PoolMeta
     __name__ = 'product.template'
-    images_resource = fields.Function(fields.Char('Images Resource'),
-        'get_images_resource')
-    images = fields.One2Many('ir.attachment', 'resource', 'Images', domain=[
+    images = fields.One2Many('ir.attachment', 'resource', 'Images',
+        filter=[
             ('product_image', '=', True),
-            ],
-        context={
-            'resource': Eval('images_resource'),
-            }, depends=['images_resource'])
-
-    def get_images_resource(self, name):
-        return 'product.template,%s' % self.id
+            ])
 
     @classmethod
     def delete(cls, templates):
